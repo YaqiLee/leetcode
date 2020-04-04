a8668ee1bc5e00af421da49ed0a7ea9db87d8f2f
liyq <liyq@gcstgroup.com>
2020-04-04 11:39:15 +0800

    8 字符串转换整数 102 二叉树的层次遍历

--- 102.二叉树的层次遍历.js
+++ 102.二叉树的层次遍历.js
@@ -20,31 +20,43 @@ var levelOrder = function (root) {
     let result = [], seq = [];
     let pL = pR = null;
 
-    seq.push(root);
+    if(!root) return [];
 
-    while(seq) {
-        
-       
-        
+    loopTreeNode(result, root, 0);
+
+    return result;
+};
+
+var loopTreeNode = function(result, node, level) {
+
+    result[level] = result[level] || []
+    result[level].push(node.val);
+
+    level++;
+
+    if(node.left) {
+        loopTreeNode(result, node.left, level);
     }
 
-
-};
+    if(node.right) {
+        loopTreeNode(result, node.right, level)
+    }
+}
 
 // @lc code=end
 
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
+// function TreeNode(val) {
+//     this.val = val;
+//     this.left = this.right = null;
+// }
 
-let root = new TreeNode(3);
-let left = new TreeNode(9);
-let right = new TreeNode(20);
-right.left = new TreeNode(15);
-right.right = new TreeNode(7);
+// let root = new TreeNode(3);
+// let left = new TreeNode(9);
+// let right = new TreeNode(20);
+// right.left = new TreeNode(15);
+// right.right = new TreeNode(7);
 
-root.left = left;
-root.right = right;
+// root.left = left;
+// root.right = right;
 
-levelOrder(root)
+// levelOrder(root)
--- 8.字符串转换整数-atoi.js
+++ 8.字符串转换整数-atoi.js
@@ -11,17 +11,33 @@
  */
 var myAtoi = function (str) {
 
+    let result = '';
+    let operate = ['+', '-'];
     let max = Math.pow(2, 31) - 1, min = -1 * Math.pow(2, 31);
 
-    str = str.trim();
-    str = parseInt(str);
+    // 先去除字符串开头空格
+    str = str.trimLeft();
 
-    if(isNaN(str)) {
+    let first = str.charAt(0);
+
+    if(isNaN(first) && !operate.includes(first)) {
         return 0;
     }
 
-    return str > max ? max : str < min ? min : str;
+    result = result + first;
 
+    for (let i = 1; i < str.length; i++) {
+        
+        if(str[i] === ' ' || isNaN(str[i])) {
+            break;
+        }
+
+        result += str[i];
+    }
+
+    return isNaN(result) ? 0 : Math.min(max, Math.max(result, min));
+    
 };
 
 // @lc code=end
+// myAtoi("-2222222 22ddd dd")
